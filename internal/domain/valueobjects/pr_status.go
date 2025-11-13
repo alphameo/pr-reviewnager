45e8c8607d5c6af30fdd75565f232af9c91196df
alphameo <pavel.2014.play@gmail.com>
2025-11-13 20:26:08 +0300

    feat(domain/valueobjects/pr_status): add ::String() and constructor ..FromString()

--- internal/domain/valueobjects/pr_status.go
+++ internal/domain/valueobjects/pr_status.go
@@ -1,8 +1,29 @@
 package valueobjects
 
+import (
+	"fmt"
+	"strings"
+)
+
 type PRStatus string
 
 const (
 	OPEN   PRStatus = "open"
 	MERGED PRStatus = "merged"
 )
+
+func NewPRStatusFromString(value string) (PRStatus, error) {
+	processed := strings.ToLower(strings.TrimSpace(value))
+	switch value {
+	case "open":
+		return OPEN, nil
+	case "merged":
+		return MERGED, nil
+	default:
+		return PRStatus(""), fmt.Errorf("no PR status: %s", processed)
+	}
+}
+
+func (s *PRStatus) String() string {
+	return string(*s)
+}
