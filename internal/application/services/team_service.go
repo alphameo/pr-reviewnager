f6cd5b625016b84437da18102fd45432e801dbe0
alphameo <pavel.2014.play@gmail.com>
2025-11-14 14:05:01 +0300

    feat(application/services/team_service): add default implementation

--- internal/application/services/team_service.go
+++ internal/application/services/team_service.go
@@ -1,10 +1,57 @@
 package services
 
 import (
+	"fmt"
+
 	"github.com/alphameo/pr-reviewnager/internal/application/dto"
+	"github.com/alphameo/pr-reviewnager/internal/application/mappers"
+	"github.com/alphameo/pr-reviewnager/internal/domain/entities"
+	r "github.com/alphameo/pr-reviewnager/internal/domain/repositories"
 )
 
 type TeamService interface {
 	CreateTeamWithUsers(teamDTO dto.CreateTeamWithUsersDTO) error
 	FindTeamByName(name string) (*dto.TeamDTO, error)
+}
+
+type DefaultTeamService struct {
+	teamRepo     r.TeamRepository
+	userRepo     r.UserRepository
+	userTeamRepo r.UserTeamRepository
+}
+
+func (s *DefaultTeamService) CreateTeamWithUsers(teamDTO dto.CreateTeamWithUsersDTO) error {
+	existingTeam, err := s.teamRepo.FindTeamByName(teamDTO.TeamName)
+	if err != nil {
+		return err
+	}
+	if existingTeam != nil {
+		return fmt.Errorf("team with name=%s already exists", teamDTO.TeamName)
+	}
+
+	team := entities.NewTeam(teamDTO.TeamName)
+	users, err := mappers.UserDTOsToEntities(teamDTO.TeamUsers)
+	if err != nil {
+		return err
+	}
+
+	for _, user := range users {
+		team.AddUser(user.ID())
+	}
+
+	s.userTeamRepo.CreateTeamAndModifyUsers(team, users)
+	return nil
+}
+
+func (s *DefaultTeamService) FindTeamByName(name string) (*dto.TeamDTO, error) {
+	team, err := s.teamRepo.FindTeamByName(name)
+	if err != nil {
+		return nil, err
+	}
+
+	if team == nil {
+		return nil, nil
+	}
+	dto := mappers.TeamToDTO(team)
+	return dto, nil
 }
