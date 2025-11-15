8cd253fb082a46de57399edaa1a2c74b66648c9b
alphameo <pavel.2014.play@gmail.com>
2025-11-16 00:35:15 +0300

    feat(infra/db/postgres): add main connection

---
+++ internal/infrastructure/db/postgres/connection.go
@@ -0,0 +1,22 @@
+// Package postgres provides repositories for PostgreSQL
+package postgres
+
+import (
+	"context"
+
+	db "github.com/alphameo/pr-reviewnager/internal/infrastructure/db/sqlc"
+	"github.com/jackc/pgx/v5"
+)
+
+func NewConnection(ctx context.Context, dsn string) (*pgx.Conn, error) {
+	conn, err := pgx.Connect(ctx, dsn)
+	if err != nil {
+		return nil, err
+	}
+
+	return conn, nil
+}
+
+func NewQueries(conn *pgx.Conn) *db.Queries {
+	return db.New(conn)
+}
