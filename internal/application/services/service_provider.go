fa40a96d111971d676d55aa051e8bcbb98d43264
alphameo <pavel.2014.play@gmail.com>
2025-11-20 20:02:30 +0300

    feat(application/services): define service provider

---
+++ internal/application/services/service_provider.go
@@ -0,0 +1,70 @@
+package services
+
+import (
+	"errors"
+	"fmt"
+
+	domainserv "github.com/alphameo/pr-reviewnager/internal/domain/services"
+	infra "github.com/alphameo/pr-reviewnager/internal/infrastructure"
+)
+
+type ServiceProvider interface {
+	UserService() UserService
+	TeamService() TeamService
+	PullRequestService() PullRequestService
+}
+
+type DefaultServiceProvider struct {
+	userServ *DefaultUserService
+	teamServ *DefaultTeamService
+	prServ   *DefaultPullRequestService
+}
+
+func NewDefaultServiceProvider(storage infra.Storage, domainServiceProvider domainserv.ServiceProvider) (*DefaultServiceProvider, error) {
+	if storage == nil {
+		return nil, errors.New("storage cannot be nil")
+	}
+	if domainServiceProvider == nil {
+		return nil, errors.New("domainServiceProvider cannot be nil")
+	}
+
+	teamServ, err := NewDefaultTeamService(
+		storage.TeamRepository(),
+		storage.UserRepository(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create team service: %w", err)
+	}
+
+	userServ, err := NewDefaultUserService(storage.UserRepository())
+	if err != nil {
+		return nil, fmt.Errorf("failed to create user service: %w", err)
+	}
+
+	prServ, err := NewDefaultPullRequestService(
+		domainServiceProvider.PullRequestDomainService(),
+		storage.PullRequestRepository(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create pull request service: %w", err)
+	}
+
+	p := DefaultServiceProvider{
+		teamServ: teamServ,
+		userServ: userServ,
+		prServ:   prServ,
+	}
+	return &p, nil
+}
+
+func (p *DefaultServiceProvider) UserService() UserService {
+	return p.userServ
+}
+
+func (p *DefaultServiceProvider) TeamService() TeamService {
+	return p.teamServ
+}
+
+func (p *DefaultServiceProvider) PullRequestService() PullRequestService {
+	return p.prServ
+}
